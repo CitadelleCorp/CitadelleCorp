eb11cbf986c98b6d6110db889d0065b98047235c
marinpousset <marin.pousset@gmail.com>
2021-02-28 01:51:49 +0100

    Update P5sketch.js
    External Hexagon

--- scripts/P5sketch.js
+++ scripts/P5sketch.js
@@ -1,21 +1,25 @@
 var canvas;
 
 let objs = [];
-let objsNum = 500;
+let objsNum = 1200;
 let MAX;
 const palette = ["#ff6f61","#ff6f61","#ff6f61","#ff6f61"];
 let maximumSize = 0.3;
 let rotSpeedValue = 3;
-let minSpeed = 3;
-let maxSpeed = 7;
+let startingSpeed = 0.03;
+let minSpeed = 80;
+let maxSpeed = 800;
+let hexSize = 0.2;
+let tooFar;
 
 function setup() {
     frameRate(30);
     canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    tooFar = windowWidth * 0.7;
   angleMode(DEGREES);
   // noStroke();
 	stroke(255);
-  MAX = min(width, height) * 0.525;
+  MAX = min(width, height) * hexSize;
 
   for (let i = 0; i < objsNum; i++) {
     objs.push(new Obj());
@@ -26,7 +30,7 @@ function draw() {
   background("#000000");
 
   lights();
-  ambientLight(150);
+  ambientLight(100);
   pointLight(color(0, 100, 0), 0, 0, 0);
 
   for (let i = 0; i < objs.length; i++) {
@@ -37,25 +41,28 @@ function draw() {
 
 class Obj {
   constructor() {
-    this.sMax = random(MAX * 0.1, MAX * maximumSize);
+    this.sMax = random(MAX * 0, MAX * maximumSize);
     this.s = this.sMax;
     this.pos = createVector(0, 0);
-    this.r = random(MAX * 0.6, MAX);
     this.t = random(360);
     this.speed = random(minSpeed, maxSpeed);
+    this.r = map(sq(sq(random(MAX, tooFar))), sq(sq(MAX)), sq(sq(tooFar)), MAX, tooFar);
+    this.currentSpeed = startingSpeed;
     this.rot = createVector(random(360), random(360), random(360));
     this.rotSpeed = createVector(random(-rotSpeedValue, rotSpeedValue), random(-rotSpeedValue, rotSpeedValue), random(-rotSpeedValue, rotSpeedValue));
     this.c = random(palette);
   }
 
   move() {
-    this.r += this.speed;
     this.rot.add(this.rotSpeed);
-    this.s = map(this.r, 0, MAX, this.sMax, 0);
+    this.s = map(sqrt(this.r), sqrt(tooFar), sqrt(MAX), this.sMax, 0);
+    this.currentSpeed = map(sq(sq(this.r)), sq(sq(MAX)), sq(sq(tooFar)), startingSpeed, this.speed);
+    this.r += this.currentSpeed;
 
     this.pos = calcPos(this.r, this.t, 6);
-    if (this.r > MAX) {
-      this.r = random(MAX * 0.6, MAX);
+
+    if (this.r > tooFar) {
+      this.r = MAX;
     }
   }
 
@@ -66,7 +73,11 @@ class Obj {
     rotateY(this.rot.y);
     rotateZ(this.rot.z);
     fill(this.c);
-    box(this.s);
+    noStroke();
+    //noFill();
+    //stroke(this.c);
+    strokeWeight(2);
+    sphere(this.s, 3, 3);
     pop();
   }
 }
