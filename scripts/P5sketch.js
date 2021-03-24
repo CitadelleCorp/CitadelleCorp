53dee5dd3a77697431d0f341588ae69c232728b5
marinpousset <marin.pousset@gmail.com>
2021-03-24 17:27:42 +0100

    Test page type

--- scripts/P5sketch.js
+++ scripts/P5sketch.js
@@ -27,7 +27,7 @@ let sphereShape = 2;
 
 let removeFadingTime = 3; // en secondes
 let startingCountdown = false;
-let removeCountdown = removeFadingTime * 30 * 2; // removeFadingTime * frameRate * sécurité
+let removeCountdown = removeFadingTime * 30; // removeFadingTime * frameRate * sécurité
 
 function isMobileDevice() {
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
